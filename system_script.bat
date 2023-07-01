879ca1a10194e0b2435f420c0b01fd7e5af2d835
Brian Espinoza <107952425+EspinozaB@users.noreply.github.com>
2023-07-01 06:21:04 -0400

    Update system_script.bat
    Updated the script to include more information.

--- system_script.bat
+++ system_script.bat
@@ -1,25 +1,88 @@
-rem This script was created for listing system specs
+rem This script was created for listing system specs, info, and more for analysis or troubleshooting.
 
 @echo off
 
-date /t >>system_script_output.txt
+echo Gathering system information...
 
-time /t >>system_script_output.txt
+(
+    echo.
+    echo === System Information ===
+    echo.
 
-hostname >>system_script_output.txt
+    date /t
+    time /t
 
-whoami >>system_script_output.txt
+    echo Hostname:
+    hostname
 
-ipconfig /all >>system_script_output.txt
+    echo Current user:
+    whoami
 
-systeminfo >>system_script_output.txt
+    echo IP Configuration:
+    ipconfig /all
 
-net user >>system_script_output.txt
+    echo System Information:
+    systeminfo
 
-net localgroup administrators >>system_script_output.txt
+    echo User Accounts:
+    net user
 
-netstat -nao -r >>system_script_output.txt
+    echo Administrators:
+    net localgroup administrators
 
-date /t >>system_script_output.txt
+    echo Network Statistics:
+    netstat -nao -r
 
-time /t  >>system_script_output.txt
+    echo Installed Programs:
+    wmic product get name, version, vendor
+
+    echo Running Services:
+    net start
+
+    echo Startup Programs:
+    wmic startup get caption, command
+
+    echo System Drivers:
+    driverquery
+
+    echo Disk Usage:
+    wmic logicaldisk get caption, description, freespace, size
+
+    echo Running Processes:
+    tasklist
+
+    echo Additional Information:
+    systeminfo /s localhost
+
+    echo Environment Variables:
+    set
+
+    echo Firewall Configuration:
+    netsh advfirewall show all
+
+    echo Installed Printers:
+    wmic printer get name, portname, drivername
+
+    echo Display Information:
+    wmic desktopmonitor get caption, screenheight, screenwidth
+
+    echo Installed Hotfixes:
+    wmic qfe get hotfixid, installedon
+
+    echo Event Logs:
+    wevtutil qe System /c:10 /rd:true /f:text /e:Events
+    wevtutil qe Application /c:10 /rd:true /f:text /e:Events
+
+    echo Disk Partition Information:
+    wmic diskdrive get Caption, Size, MediaType
+
+    echo.
+    echo === End of System Information ===
+    echo.
+
+    echo Elapsed Time: %time%
+) >>system_script_output.txt
+
+echo System information has been successfully gathered and saved to system_script_output.txt.
+
+pause
